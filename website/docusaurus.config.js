a014031c00d16c7dd2d6a78b47a2deb7d0446747
Tushar <tdebnath@expedia.com>
2019-11-26 15:23:17 +0530

    changes

---
+++ website/docusaurus.config.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  title: 'ProtoKit',
+  tagline: 'Prototyping for everyone',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Expedia', // Usually your GitHub org/user name.
+  projectName: 'cosmos', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'ProtoKit',
+      logo: {
+        alt: 'ProtoKit Logo',
+        src: 'img/logo.svg',
+      },
+      links: [
+        {to: 'docs/doc1', label: 'Docs', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
+       
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Docs',
+              to: 'docs/doc1',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+          ],
+        },
+      ],
+      
+      copyright: `Copyright © ${new Date().getFullYear()} Expedia Group. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+          themes: ['@docusaurus/theme-live-codeblock'],
+        },
+      },
+    ],
+  ],
+};
---
+++ website/sidebars.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  docs: {
+    'Foundation': ['mdx'],
+    'Components': ['mdx'],
+    'Views': ['doc1', 'doc2', 'doc3'],
+    Animation: ['mdx'],
+    Utilities: ['mdx'],
+  },
+};
