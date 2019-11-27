8ee07453d6e999bb0a9bda05b551ddc680328896
Tushar <tdebnath@expedia.com>
2019-11-27 22:31:49 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,14 @@ import React from "react";
 import Test from '../docs/test.mdx'
 import {MDXProvider} from '@mdx-js/react'
 
-const components = {
-  h1: props => <h1 style={{color: 'tomato'}} {...props} />
-}
+
 
 
 
 const Home = () => {
   return(
 
-<MDXProvider components={components}>
+<MDXProvider>
     <Test /> 
 </MDXProvider>
 
