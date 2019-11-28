82409e6e3232c6af9172c1dc949818cbb376ba1d
Tushar <tdebnath@expedia.com>
2019-11-28 22:20:45 +0530

    changes

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -69,7 +69,7 @@ function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
+        <div className="container" >
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
@@ -84,19 +84,7 @@ function Home() {
           </div>
         </div>
       </header>
-      <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
+     
     </Layout>
   );
 }
